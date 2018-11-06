1c0b42d37d9e5dbc46d7891aa2301cb203da36bf
derek <derek_merck@brown.edu>
2018-11-05 21:40:55 -0500

    memory for study or series level queries

--- apps/cli/utils/arg_utils.py
+++ apps/cli/utils/arg_utils.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-import os, json, yaml, logging
+import os, json, yaml, logging, re
 from glob import glob
 
 def add_service_opts(parser: ArgumentParser):
@@ -16,7 +16,21 @@ def get_services(opts):
     services = {}
     if opts.get('services_file'):
         with open(opts.get('services_file')) as f:
+            path_matcher = re.compile(r'\$\{([^}^{]+)\}')
+
+            # variable expansion -- see https://stackoverflow.com/a/52412796
+            def path_constructor(loader, node):
+                ''' Extract the matched value, expand env variable, and replace the match '''
+                value = node.value
+                match = path_matcher.match(value)
+                env_var = match.group()[2:-1]
+                return os.environ.get(env_var) + value[match.end():]
+
+            yaml.add_implicit_resolver('!path', path_matcher)
+            yaml.add_constructor('!path', path_constructor)
+
             services.update(yaml.safe_load(f))
+
     if opts.get('services_env'):
         services.update(yaml.safe_load(os.environ.get(opts.get('services_env'))))
     if opts.get('services_dir'):
