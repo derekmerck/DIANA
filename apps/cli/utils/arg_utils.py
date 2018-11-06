3edc63d42a0e5255bbcd7422c974f2f2028cfe8f
derek <derek_merck@brown.edu>
2018-11-06 11:40:12 -0500

    variable expansion on services yaml

--- apps/cli/utils/arg_utils.py
+++ apps/cli/utils/arg_utils.py
@@ -3,6 +3,7 @@ import os, json, yaml, logging, re
 from glob import glob
 from pprint import pformat
 
+
 def add_service_opts(parser: ArgumentParser):
     parser.add_argument("-s", "--services_file",
                     help="Service configuration yaml file")
@@ -13,33 +14,25 @@ def add_service_opts(parser: ArgumentParser):
     parser.add_argument("--dump", action="store_true",
                     help="Dump configuration as yaml and json")
 
+
 def get_services(opts):
-    path_matcher = re.compile(r'\$\{([^}^{]+)\}')
-
-    # variable expansion -- see https://stackoverflow.com/a/52412796
-    def path_constructor(loader, node):
-        ''' Extract the matched value, expand env variable, and replace the match '''
-        value = node.value
-        match = path_matcher.match(value)
-        env_var = match.group()[2:-1]
-        return os.environ.get(env_var) + value[match.end():]
-
-    yaml.add_implicit_resolver('!path', path_matcher)
-    yaml.add_constructor('!path', path_constructor)
 
     services = {}
     if opts.get('services_file'):
         with open(opts.get('services_file')) as f:
-            services.update(yaml.safe_load(f))
+            ff = os.path.expandvars(f)
+            services.update(yaml.safe_load(ff))
     if opts.get('services_env'):
-        services.update(yaml.safe_load(os.environ.get(opts.get('services_env'))))
+        ff = os.path.expandvars(os.environ.get(opts.get('services_env')))
+        services.update(yaml.safe_load(ff))
     if opts.get('services_dir'):
         logging.debug(glob(os.path.join(opts.get('services_dir'), "*.yml")))
         fps = glob(os.path.join(opts.get('services_dir'), "*.yml"))
         for fp in fps:
             logging.debug(fp)
             with open(fp) as f:
-                services.update(yaml.safe_load(f))
+                ff = os.path.expandvars(f)
+                services.update(yaml.safe_load(ff))
 
     logging.debug(pformat(services))
 
